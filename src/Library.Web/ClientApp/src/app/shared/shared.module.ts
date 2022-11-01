20206443323faf6ed792cb0add71544411f45257
Yiyi Chen <yiyi.chen@viable.at>
2022-11-01 09:34:00 +0100

    chore: [LIB-#17] configure shared module

---
+++ src/Library.Web/ClientApp/src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from './material/material.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  providers: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MaterialModule,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MaterialModule,
+  ],
+})
+export class SharedModule {}
