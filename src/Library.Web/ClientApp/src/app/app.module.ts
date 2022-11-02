3e295d2267de583908e525ba309607e4f66713b8
Yiyi Chen <yiyi.chen@viable.at>
2022-11-02 17:47:52 +0100

    feat: [LIB-#29] add spinner

--- src/Library.Web/ClientApp/src/app/app.module.ts
+++ src/Library.Web/ClientApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BookModule } from './features/book/book.module';
 import { HomeModule } from './features/home/home.module';
+import { InterceptorsModule } from './interceptors/interceptors.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -18,6 +19,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     CoreModule,
     HomeModule,
+    InterceptorsModule,
     SharedModule,
   ],
   providers: [],
