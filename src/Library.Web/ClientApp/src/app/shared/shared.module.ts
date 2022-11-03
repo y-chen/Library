48da8b16114972be9935bea3e87916f2c03befb0
Yiyi Chen <yiyi.chen@viable.at>
2022-11-03 06:30:40 +0100

    chore: [LIB-#31] clean up

--- src/Library.Web/ClientApp/src/app/shared/shared.module.ts
+++ src/Library.Web/ClientApp/src/app/shared/shared.module.ts
@@ -5,25 +5,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 
-import { FullScreenSpinnerComponent } from './components/full-screen-spinner/full-screen-spinner.component';
 import { OverlaySpinnerComponent } from './components/overlay-spinner/overlay-spinner.component';
-import { ShellComponent } from './components/shell/shell.component';
-import { NavItemComponent } from './components/shell/sidebar/nav-item/nav-item.component';
-import { SidebarComponent } from './components/shell/sidebar/sidebar.component';
-import { ToolbarComponent } from './components/shell/toolbar/toolbar.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ValidationErrorsComponent } from './components/validation-errors/validation-errors.component';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [
-    FullScreenSpinnerComponent,
-    NavItemComponent,
-    OverlaySpinnerComponent,
-    ShellComponent,
-    SidebarComponent,
-    ToolbarComponent,
-    ValidationErrorsComponent,
-  ],
+  declarations: [OverlaySpinnerComponent, ToolbarComponent, ValidationErrorsComponent],
   providers: [],
   imports: [
     CommonModule,
@@ -40,11 +28,7 @@ import { MaterialModule } from './material/material.module';
     RouterModule,
     MaterialModule,
 
-    FullScreenSpinnerComponent,
-    NavItemComponent,
     OverlaySpinnerComponent,
-    ShellComponent,
-    SidebarComponent,
     ToolbarComponent,
     ValidationErrorsComponent,
   ],
